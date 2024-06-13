89067ffd3419507f8d87b18831cf9408ed85397a
chohyunki <dmsrlgusrl@naver.com>
2024-06-13 16:38:35 +0900

    fix: fix testcode

--- blockchain/contracts/CopyRight.sol
+++ blockchain/contracts/CopyRight.sol
@@ -8,6 +8,7 @@ contract CopyRight is Signer{
     uint256 public contractId;
 
     struct ContractContents {
+        RightType rightType;
         address author;
         bytes contents;
         string url;
@@ -15,30 +16,41 @@ contract CopyRight is Signer{
         uint256 updatedBlocknumber;
     }
 
-    enum type {
-        
+    enum RightType {
+        CopyRight,
+        Design,
+        TradeMark
     }
 
     mapping (uint256 => ContractContents) public contractInfos;
     mapping (address => uint256[] ownedContractIds) public userContracts;
 
-    event Registerd(uint256 indexed contractId, address indexed author, bytes contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
-    event Changed(uint256 indexed contractId, address indexed author, bytes contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
+    event Registerd(RightType rightType, uint256 indexed contractId, address indexed author, bytes contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
+    event Changed(RightType rightType, uint256 indexed contractId, address indexed author, bytes contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
     constructor() Signer(msg.sender) {}
 
-    function register(bytes memory contents, string memory url, bytes32 contentsHash) public {
-        ContractContents memory contractContents = ContractContents(msg.sender, contents, url, contentsHash, block.number);
+    function register(RightType rightType,bytes memory contents, string memory url, bytes32 contentsHash) public {
+        ContractContents memory contractContents = ContractContents(rightType,msg.sender, contents, url, contentsHash, block.number);
         contractInfos[contractId] = contractContents;
         userContracts[msg.sender].push(contractId);
-        emit Registerd(contractId ,msg.sender, contents, url, contentsHash, block.number);
+        emit Registerd(rightType, contractId ,msg.sender, contents, url, contentsHash, block.number);
         contractId++;
     }
 
-    function change(bytes memory contents, string memory url, bytes32 contentsHash) public isSigner {
-        ContractContents memory contractContents = ContractContents(msg.sender, contents, url, contentsHash, block.number);
+    function change(RightType rightType,bytes memory contents, string memory url, bytes32 contentsHash) public isSigner {
+        ContractContents memory contractContents = ContractContents(rightType,msg.sender, contents, url, contentsHash, block.number);
         contractInfos[contractId] = contractContents;
         userContracts[msg.sender].push(contractId);
-        emit Registerd(contractId ,msg.sender, contents, url, contentsHash, block.number);
+        emit Registerd(rightType, contractId ,msg.sender, contents, url, contentsHash, block.number);
         contractId++;
+    }
+
+    function getContractContents(uint256 _contractId) public view returns (RightType, address, bytes memory, string memory, bytes32, uint256) {
+        ContractContents memory content = contractInfos[_contractId];
+        return (content.rightType, content.author, content.contents, content.url, content.contentsHash, content.updatedBlocknumber);
+    }
+
+    function decodeContents(bytes memory contents) public pure returns (string memory) {
+        return string(contents);
     }
 }
--- blockchain/test/CopyRight.js
+++ blockchain/test/CopyRight.js
@@ -26,7 +26,8 @@ describe("CopyRight", function () {
   describe("Register", function () {
     it("Should register a new contract correctly", async function () {
       const { copyRight, owner } = await loadFixture(deployCopyRightFixture);
-      const name = "Author Name";
+      const type = 0;
+      const author = owner.address;
       const metadata = "data from http://example.com";
       const metadataString = JSON.stringify(metadata);
       const contents = ethers.toUtf8Bytes(metadataString);
@@ -35,15 +36,29 @@ describe("CopyRight", function () {
       const contentsHash = ethers.solidityPackedKeccak256(["string"],[metadataString]);
       console.log("contentsHash >>", contentsHash);
 
-      await expect(copyRight.register(name, contents, url, contentsHash))
-      .to.emit(copyRight, "Registerd")
-      .withArgs(0, name, contents, url, contentsHash);
-      
-      const contractInfo = await copyRight.contractInfos(0);
-      expect(contractInfo.author).to.equal(name);
-      expect(ethers.hexlify(contractInfo.contents)).to.equal(ethers.hexlify(contents));
-      expect(contractInfo.url).to.equal(url);
-      expect(contractInfo.contentsHash).to.equal(contentsHash);
+      const tx = await copyRight.register(type, contents, url, contentsHash);
+      const receipt = await tx.wait();
+      console.log(receipt.logs)
+
+      const eventLog = receipt.logs.map(log => log.args);
+      console.log("eventLog >>", eventLog);
+      const [eventRightType, eventContractId, eventAuthor, eventContents, eventUrl, eventContentsHash, eventUpdatedBlocknumber] = eventLog[0];
+
+      expect(eventRightType).to.equal(type);
+      expect(eventContractId).to.equal(0);
+      expect(eventAuthor).to.equal(owner.address);
+      expect(ethers.hexlify(eventContents)).to.equal(ethers.hexlify(contents));
+      expect(eventUrl).to.equal(url);
+      expect(eventContentsHash).to.equal(contentsHash);
+      expect(eventUpdatedBlocknumber).to.equal(receipt.blockNumber);
+
+      const contractInfo = await copyRight.getContractContents(0);
+      expect(contractInfo[0]).to.equal(type);
+      expect(contractInfo[1]).to.equal(owner.address);
+      expect(ethers.hexlify(contractInfo[2])).to.equal(ethers.hexlify(contents));
+      expect(contractInfo[3]).to.equal(url);
+      expect(contractInfo[4]).to.equal(contentsHash);
+      expect(contractInfo[5]).to.equal(receipt.blockNumber);
     });
   });
 });
