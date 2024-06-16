d44224c7e6cdc3f90473f4f0666452b06a61e9a1
chohyunki <dmsrlgusrl@naver.com>
2024-06-16 22:31:04 +0900

    fix: add nft owner with copyright

--- blockchain/contracts/CopyRight.sol
+++ blockchain/contracts/CopyRight.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "./Signer.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract CopyRight is Signer{
 
@@ -24,6 +25,7 @@ contract CopyRight is Signer{
 
     mapping (uint256 => ContractContents) public contractInfos;
     mapping (address => uint256[] ownedContractIds) public userContracts;
+    mapping (uint256 => address[] nftlist) public registerdNft;
 
     event Registerd(RightType rightType, uint256 indexed contractId, address indexed author, string contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
     event Changed(RightType rightType, uint256 indexed contractId, address indexed author, string contents, string url, bytes32 contentsHash, uint256 indexed updatedBlocknumber);
@@ -52,5 +54,16 @@ contract CopyRight is Signer{
 
     function checkContractIdByAddress(address _address) public view returns (uint256 [] memory) {
         return userContracts[_address];
+    }
+
+    function registerNft(address _nftAddress, uint256 _nftId, uint256 _rightId) public {
+        require(contractInfos[_rightId].author == msg.sender, "Only the author can register the NFT");
+        IERC721 nftContract = IERC721(_nftAddress);
+        require(nftContract.ownerOf(_nftId) == msg.sender, "You must own the NFT to register it");
+        registerdNft[_rightId].push(_nftAddress);
+    }
+
+    function getRegisteredNfts(uint256 _rightId) public view returns (address[] memory) {
+        return registerdNft[_rightId];
     }
 }
